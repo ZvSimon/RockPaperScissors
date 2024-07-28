5d3d0d66791efbc2236815928a6685faa4f7a518
Simon <73712759+ZvSimon@users.noreply.github.com>
2024-07-28 13:56:38 +0200

    Opti and anonymous functions

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
-const start = function () {
-    console.log('Game is starting...');
-}
+// const start = function () {
+//     console.log('Game is starting...');
+// }
 
 console.dir(start);
-startGameBtn.addEventListener('click', start);
+startGameBtn.addEventListener('click', function () {
+    console.log('Game is starting...')}
+);
+
 
